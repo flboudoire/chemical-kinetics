96a22abd4acebac8cb296545c82652cd95637e97
flboudoire <flboudoire@gmail.com>
2020-01-30 16:37:10 +0100

    Auto update

--- chemical_kinetics/data_processing.py
+++ chemical_kinetics/data_processing.py
@@ -17,22 +17,22 @@ class Dataset:
             passed vs time files, if this argument is not set
             further fitting will proceed without taking the charge
             passed in account.
-        t_label,c_label,q_label (str, optional):
+        t_label|c_label|q_label (str, optional):
             Set the corresponding attributes values. Used to set the
             x and y axes labels when plotting.
     
     Attributes:
-        df_c, df_c_std, df_c_fit:
+        df_c|df_c_std|df_c_fit:
             DataFrames used to store respectively the mean
             concentration, the concentration standard deviations and the
             corresponding fit results. Defined by the load_c method or
             directly at initialization using the files_c argument.
-        df_q, df_q_std, df_q_fit:
+        df_q|df_q_std|df_q_fit:
             DataFrames used to store respectively the mean charge
             passed, the charge passed standard deviations and the
             corresponding fit results. Defined by the load_q method or
             directly at initialization using the files_q argument.
-        t_label, c_label, q_label (str):
+        t_label|c_label|q_label (str):
             Labels to be used for the x and y axes when plotting the
             datasets.
         fit_results:
