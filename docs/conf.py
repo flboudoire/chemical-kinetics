dba55e25736eccd730486d3e5f978bd2349c440a
flboudoire <flboudoire@gmail.com>
2020-01-30 16:35:43 +0100

    Auto update

--- docs/conf.py
+++ docs/conf.py
@@ -43,17 +43,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = False
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_use_keyword = True
-napoleon_custom_sections = None
 
 # -- Options for HTML output -------------------------------------------------
 
