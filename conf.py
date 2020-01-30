186a62343070c0ae736901849bb365c2353341d8
flboudoire <flboudoire@gmail.com>
2020-01-30 14:44:03 +0100

    Update

--- conf.py
+++ conf.py
@@ -32,12 +32,7 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-'recommonmark',
-'sphinx.ext.autodoc',
-'sphinxcontrib.napoleon',
-"sphinx_rtd_theme"
-]
+extensions = ['sphinx.ext.autodoc','sphinxcontrib.napoleon','sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
