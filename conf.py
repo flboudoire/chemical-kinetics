e392a4e4b09911597855e4a8351864a580ad018b
flboudoire <flboudoire@gmail.com>
2020-01-30 14:19:18 +0100

    Update

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ release = '2020'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
