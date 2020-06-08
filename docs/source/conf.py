4a595d9d7a725e63e3a5b61e7795212094cc35b6
flboudoire <anthos@pop-os.localdomain>
2020-06-08 15:30:54 +0200

    Auto update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.autosectionlabel'
 ]
 
-autodoc_mock_imports = ['scipy', 'pandas', 'matplotlib']
+autodoc_mock_imports = ['scipy', 'pandas', 'matplotlib', 'numpy']
 
 # source_suffix = '.rst'
 source_suffix = '.rst'
